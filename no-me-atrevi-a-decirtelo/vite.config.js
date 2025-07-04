690857769e61dfcee65a31f6898d08849f5d5d89
Renato <renato.yanez2@mail.udp.cl>
2025-07-04 18:32:32 -0400

    Configurando viteconfig para que acepte a railawy para desplegar

--- no-me-atrevi-a-decirtelo/vite.config.js
+++ no-me-atrevi-a-decirtelo/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['frontend-production-87d1.up.railway.app']
+  }
 })
